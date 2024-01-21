a6a18f9899d32c8223a4682d0fad581e14170ad6
Jakub Arnold <darthdeus@gmail.com>
2024-01-21 16:09:36 +0100

    Working version with multiple font variants

---
+++ README.md
@@ -0,0 +1,18 @@
+# Fantasque Sans Mono Compact
+
+A small fontforge python script for adjusting the width of [Fantasque Sans Mono](https://github.com/belluzj/fantasque-sans).
+
+The main reason for this repo is that IntelliJ IDEs don't allow changing the font letter spacing. It might be possible to do this in a better way (related issue https://github.com/belluzj/fantasque-sans/issues/164), but I just wanted something quick and dirty that'd work. Here's the related issue for IntelliJ https://youtrack.jetbrains.com/issue/JBR-5247/Allow-to-change-the-letterspacing-of-a-font but I have little faith in that changing since it's been open for 9 years.
+
+However, the solution in this repo seems to work very well. Only downside is that it requires the user to generate the font themselves unless want the same value that I generated :) (which is 90% width).
+
+## Author and license
+
+Fantasque Sans Mono Created by Jany Belluz \<jany.belluz AT hotmail.fr\>
+
+Licensed under the SIL Open Font License (see [LICENSE.txt](LICENSE.txt)).
+
+Please send me an e-mail or [report an issue on
+Github](http://github.com/belluzj/cosmic-sans-neue/issues) if you stumble upon
+bad design or rendering problems (with screen shot if possible), or if you need
+more characters, or if you want to compliment me (I love compliments).
--- change_spacing.py
+++ change_spacing.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env fontforge
 import fontforge
-font = fontforge.open('FantasqueCompact-Regular.ttf')
 
-font.fontname = "FantasqueCompact-Regular"
-font.familyname = "Fantasque Compact"
-font.fullname = "Fantasque Compact Regular"
+src_name_template = "src-font/FantasqueSansMono-Regular.ttf"
 
-new_unique_id = "Fantasquecompact; 2024; Jakub Arnold"
+out_family_name = "Fantasque Compact"
+author = "Jakub Arnold"
 
-font.appendSFNTName('English (US)', 'UniqueID', new_unique_id)
-font.version = "0.1.0"
+for variant in ["Bold", "BoldItalic", "Italic", "Regular"]:
+    src_name = src_name_template.replace("Regular", variant)
+    out_short_family_name = out_family_name.replace(" ", "")
 
-# Reduce the letter spacing by 10%
-for glyph in font.glyphs():
-    if glyph.isWorthOutputting():
-        glyph.width = int(glyph.width * 0.9)
+    print(f"Processing {src_name}")
 
-font.generate('out/FantasqueCompact-Regular.ttf')
-font.close()
+    font = fontforge.open(src_name)
+
+    font.fontname = f"{out_short_family_name}-{variant}"
+    font.familyname = out_family_name
+    font.fullname = out_family_name + " " + variant
+
+    new_unique_id = f"{out_short_family_name}; 2024; {author}"
+
+    font.appendSFNTName('English (US)', 'UniqueID', new_unique_id)
+    font.version = "0.1.0"
+
+    # Reduce the letter spacing by 10%
+    for glyph in font.glyphs():
+        if glyph.isWorthOutputting():
+            glyph.width = int(glyph.width * 0.9)
+
+    result_path = f"out/{out_short_family_name}-{variant}.ttf"
+
+    print(f"Generating {result_path}")
+
+    font.generate(result_path)
+    font.close()
